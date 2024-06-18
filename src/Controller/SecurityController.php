e56b4169744ad432bf3d7431b59bb2518d6e9a6c
Matéo <mslyemi@gmail.com>
2024-06-18 16:27:25 +0200

    add alicebundle + modify and delete posts

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -7,7 +7,6 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
-use App\Form\UserType;
 use App\Entity\User;
 
 
